a616b29c8691b4e2eeac0f737c8de745fc056b16
Khairul Akmal <haxl.lab@gmail.com>
2022-09-30 11:55:24 +0800

    feat: add image assets url domain

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["assets.pokemon.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
