17cb8d52355afbeba2c612c1b257a8c9466d4da7
Khairul Akmal <haxl.lab@gmail.com>
2022-09-30 13:13:24 +0800

    fix: use image/next component

--- components/Jumbo.jsx
+++ components/Jumbo.jsx
@@ -2,10 +2,16 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Jumbo = ({ image }) => (
-  <header className="d-flex flex-row justify-content-center mt-4 mb-4">
+  <header className="d-flex flex-row justify-content-center mt-5 mb-5 h-100 w-100">
     <Link href="/">
       <a>
-        <img src={image} className={"img-fluid"} />
+        <Image
+          src={image}
+          width={380}
+          height={140}
+          layout={"fixed"}
+          alt="Pokedex Logo"
+        />
       </a>
     </Link>
   </header>
